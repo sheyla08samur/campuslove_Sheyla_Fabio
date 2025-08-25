20f03c349ab12e15a95d70a3d58e38a0e038c7ba
fabioo-hm <fabixhernandez20@gmail.com>
2025-08-25 14:37:38 -0500

    style: :lipstick: Improve Main Menu style

--- src/UI/MenuHandler.cs
+++ src/UI/MenuHandler.cs
@@ -17,64 +17,37 @@ namespace Campuslove_Sheyla_Fabio.src.UI
                     new FigletText("¡ CAMPUSLOVE !")
                     .Centered()
                     .Color(Color.Pink1));
-                AnsiConsole.Write(new Rule("[bold purple]\n❤️ Encuentra el amor de tu vida con CampusLove ❤️[/]").Centered());
+                AnsiConsole.Write(new Rule("[bold purple]\n❤️  Encuentra el amor de tu vida con CampusLove ❤️  [/]").Centered());
 
-                AnsiConsole.MarkupLine("[bold blue] Menu Principal[/]");
-                AnsiConsole.MarkupLine("[bold]Elije la opción que deseas gestionar:\n[/]");
+                AnsiConsole.MarkupLine("[bold blue]Menu Principal[/]");
 
-                var m_handler = new Table();
-                m_handler.Border = TableBorder.Rounded;
+                var selection = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                    .Title("[bold]Seleccione una opción[/]")
+                    .AddChoices(new[]
+                    {
+                        "0. Crear Cuenta",
+                        "1. Iniciar Sesión",
+                        "2. Salir"
+                    }));
 
-                m_handler.AddColumn("#");
-                m_handler.AddColumn("Opción");
-                m_handler.AddRow("1", "Crear Torneo");
-                m_handler.AddRow("2", "Registro Equipos");
-                m_handler.AddRow("3", "Registro Jugadores");
-                m_handler.AddRow("4", "Transferencia (Compra, Prestamo)");
-                m_handler.AddRow("5", "Estadisticas");
-                m_handler.AddRow("6", "Salir");
-
-                AnsiConsole.Write(m_handler);
-
-                int opc = AnsiConsole.Ask<int>("[bold blue]Ingrese la opción deseada: [/]");
-                switch (opc)
+                switch (selection[0])
                 {
-                    case 1:
-
+                    case '0':
                         LoadingAnimation.MostrarAnimacionCarga("Espera un momento...");
                         AnsiConsole.Clear();
                         // Mostrar menu
                         break;
-
-                    case 2:
-                        LoadingAnimation.MostrarAnimacionCarga("Espera un momento...");
-                        AnsiConsole.Clear();
-                        // Mostrar menu
-
-                        break;
-                    case 3:
+                    case '1':
                         LoadingAnimation.MostrarAnimacionCarga("Espera un momento...");
                         AnsiConsole.Clear();
                         // Mostrar menu
                         break;
-                    case 4:
-                        LoadingAnimation.MostrarAnimacionCarga("Espera un momento...");
-                        AnsiConsole.Clear();
-                        // Mostrar menu
-                        break;
-                    case 5:
-                        LoadingAnimation.MostrarAnimacionCarga("Espera un momento...");
-                        AnsiConsole.Clear();
-                        // Mostrar menu
-                        break;
-                    case 6:
-                        // Salir del programa
-                        //AnimacionCarga.MostrarAnimacionCarga();
+                    case '2':
                         AnsiConsole.Clear();
                         AnsiConsole.MarkupLine("[bold red]¡Gracias por usar CampusLove![/]");
                         Environment.Exit(0);
                         break;
-
                     default:
                         AnsiConsole.MarkupLine("[bold red]Opción no válida. Por favor, intente de nuevo.[/]");
                         Thread.Sleep(2000);
